c8726eb362f2ca9ae2fc84797f910d8f552424d2
lsby <2451759073@qq.com>
2026-01-12 10:15:04 +0800

    适配新框架

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import fileUpload from 'express-fileupload'
 import { z } from 'zod'
 import { 插件 } from '@lsby/net-core'
+import { Right } from '@lsby/ts-fp-data'
 
-var 类型描述 = z.object({
+var 错误类型描述 = z.never()
+var 正确类型描述 = z.object({
   files: z
     .object({
       name: z.string(),
@@ -19,9 +21,9 @@ var 类型描述 = z.object({
   filePayload: z.string().optional(),
 })
 
-export class 文件上传插件 extends 插件<typeof 类型描述> {
+export class 文件上传插件 extends 插件<typeof 错误类型描述, typeof 正确类型描述> {
   constructor(opt: { 文件最大大小: number }) {
-    super(类型描述, async (req, res) => {
+    super(错误类型描述, 正确类型描述, async (req, res) => {
       await new Promise((resP, _rej) =>
         fileUpload({ limits: { fileSize: opt.文件最大大小 } })(req, res, () => {
           resP(null)
@@ -36,10 +38,10 @@ export class 文件上传插件 extends 插件<typeof 类型描述> {
         }
       }
 
-      return {
+      return new Right({
         files: Object.values(req.files as unknown as fileUpload.UploadedFile[]),
         filePayload,
-      }
+      })
     })
   }
 }
